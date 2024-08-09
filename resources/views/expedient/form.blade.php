8184115a6354fc49254bf472b1ba068f05869ade
Millos63 <millosespinosa@gmail.com>
2024-08-09 09:31:56 -0600

    Commit antes del cambio de estilos

--- resources/views/expedient/form.blade.php
+++ resources/views/expedient/form.blade.php
@@ -2,55 +2,55 @@
     <div class="col-md-12">
         
         <!--FORMULARIO PARA CLIENTES-->
-        <div>
-            
-        </div>
-        <div class="form-group mb-2 mb20 ">
-            <label for="id_client" class="form-label">{{ __('id_client') }}</label>
-            <select name="id_client" class="form-control @error('id_client') is-invalid @enderror"  id="id_client" placeholder="Id Client" onchange="toggleClientFields()">
-                @foreach($clients as $client)
-                    <option value="{{$client->id}}"{{ old('id_client', $expedient?->id_client) == $client->id ? 'selected' : ''}}>
-                        {{$client->first_name}}
-                        {{$client->last_name}}
-                    </option>   
-                @endforeach
-                <option value = 'new_client'> Crear nuevo cliente </option>
-            </select>
-            {!! $errors->first('id_client', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
+        <div style="border-radius: 2px">
+            <div class="form-group mb-2 mb20 ">
+                <label for="id_client" class="form-label">{{ __('id_client') }}</label>
+                <select name="id_client" class="form-control @error('id_client') is-invalid @enderror"  id="id_client" placeholder="Id Client" onchange="toggleClientFields()">
+                    @foreach($clients as $client)
+                        <option value="{{$client->id}}"{{ old('id_client', $expedient?->id_client) == $client->id ? 'selected' : ''}}>
+                            {{$client->first_name}}
+                            {{$client->last_name}}
+                        </option>   
+                    @endforeach
+                    <option value = 'new_client'> Crear nuevo cliente </option>
+                </select>
+                {!! $errors->first('id_client', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
 
-        <!-- Campos para detalles del cliente -->
-        <div class="form-group mb-2 mb20">
-            <label for="first_name" class="form-label">{{ __('First Name') }}</label>
-            <input type="text" name="first_name" class="form-control @error('first_name') is-invalid @enderror" value="{{ old('first_name', $client?->first_name) }}" id="first_name" placeholder="First Name" readonly>
-            {!! $errors->first('first_name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
+            <!-- Campos para detalles del cliente -->
+            <div class="form-group mb-2 mb20">
+                <label for="first_name" class="form-label">{{ __('First Name') }}</label>
+                <input type="text" name="first_name" class="form-control @error('first_name') is-invalid @enderror" value="{{ old('first_name', $client?->first_name) }}" id="first_name" placeholder="First Name" readonly>
+                {!! $errors->first('first_name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
 
-        <div class="form-group mb-2 mb20">
-            <label for="last_name" class="form-label">{{ __('Last Name') }}</label>
-            <input type="text" name="last_name" class="form-control @error('last_name') is-invalid @enderror" value="{{ old('last_name', $client?->last_name) }}" id="last_name" placeholder="Last Name" readonly>
-            {!! $errors->first('last_name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            <div class="form-group mb-2 mb20">
+                <label for="last_name" class="form-label">{{ __('Last Name') }}</label>
+                <input type="text" name="last_name" class="form-control @error('last_name') is-invalid @enderror" value="{{ old('last_name', $client?->last_name) }}" id="last_name" placeholder="Last Name" readonly>
+                {!! $errors->first('last_name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
+            <div class="form-group mb-2 mb20">
+                <label for="direction" class="form-label">{{ __('Direction') }}</label>
+                <input type="text" name="direction" class="form-control @error('direction') is-invalid @enderror" value="{{ old('direction', $client?->direction) }}" id="direction" placeholder="Direction" readonly>
+                {!! $errors->first('direction', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
+            <div class="form-group mb-2 mb20">
+                <label for="gender" class="form-label">{{ __('Gender') }}</label>
+                <input type="text" name="gender" class="form-control @error('gender') is-invalid @enderror" value="{{ old('gender', $client?->gender) }}" id="gender" placeholder="Gender" readonly>
+                {!! $errors->first('gender', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
+            <div class="form-group mb-2 mb20">
+                <label for="phone_number" class="form-label">{{ __('Phone Number') }}</label>
+                <input type="text" name="phone_number" class="form-control @error('phone_number') is-invalid @enderror" value="{{ old('phone_number', $client?->phone_number) }}" id="phone_number" placeholder="Phone Number" readonly>
+                {!! $errors->first('phone_number', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
+            <div class="form-group mb-2 mb20">
+                <label for="age" class="form-label">{{ __('Age') }}</label>
+                <input type="text" name="age" class="form-control @error('age') is-invalid @enderror" value="{{ old('age', $client?->age) }}" id="age" placeholder="Age" readonly>
+                {!! $errors->first('age', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+            </div>
         </div>
-        <div class="form-group mb-2 mb20">
-            <label for="direction" class="form-label">{{ __('Direction') }}</label>
-            <input type="text" name="direction" class="form-control @error('direction') is-invalid @enderror" value="{{ old('direction', $client?->direction) }}" id="direction" placeholder="Direction" readonly>
-            {!! $errors->first('direction', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="gender" class="form-label">{{ __('Gender') }}</label>
-            <input type="text" name="gender" class="form-control @error('gender') is-invalid @enderror" value="{{ old('gender', $client?->gender) }}" id="gender" placeholder="Gender" readonly>
-            {!! $errors->first('gender', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="phone_number" class="form-label">{{ __('Phone Number') }}</label>
-            <input type="text" name="phone_number" class="form-control @error('phone_number') is-invalid @enderror" value="{{ old('phone_number', $client?->phone_number) }}" id="phone_number" placeholder="Phone Number" readonly>
-            {!! $errors->first('phone_number', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
-        <div class="form-group mb-2 mb20">
-            <label for="age" class="form-label">{{ __('Age') }}</label>
-            <input type="text" name="age" class="form-control @error('age') is-invalid @enderror" value="{{ old('age', $client?->age) }}" id="age" placeholder="Age" readonly>
-            {!! $errors->first('age', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
+       
 
     <!--FORMULARIO PARA EXPEDIENTE-->
     
